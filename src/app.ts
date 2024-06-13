a291f3f8151a5df97665249ed173cba57fd17470
xleat <xleat@outlook.com>
2024-06-13 21:24:20 +0800

    Modify: 全局error log文件记录

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import {TelegramBotClient} from './client/TelegramBotClient'
 import {WeChatClient} from './client/WechatClient'
+import {LogUtils} from './utils/LogUtils'
 
+process.on('uncaughtException', (err) => {
+    LogUtils.config().getLogger('error').error('wechat2Tg uncaughtException', err)
+})
 
 const bot = TelegramBotClient.getInstance()
 bot.init()
