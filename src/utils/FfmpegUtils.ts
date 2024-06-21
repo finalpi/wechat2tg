eb8fa777fa083c9f8eeaa33d9bc49a4650cc55a8
finalpi <46010917+finalpi@users.noreply.github.com>
2024-06-21 09:29:30 +0800

    FIX: 解决大于1mb的gif发送失败的问题

--- src/utils/FfmpegUtils.ts
+++ src/utils/FfmpegUtils.ts
@@ -4,7 +4,6 @@ import TgsUtils from './TgsUtils'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ffmpeg = require('fluent-ffmpeg')
 
-
 export class ConverterHelper {
     constructor() {
         // 设置 ffmpeg-static 的路径
@@ -12,20 +11,45 @@ export class ConverterHelper {
     }
 
     async webmToGif(inputFile: string | Buffer, outputFile: string): Promise<void> {
+        const targetFileSize = 1 * 1024 * 1024 // 1MB
+
         return new Promise((resolve, reject) => {
-            ffmpeg()
-                .input(inputFile)
-                // .outputOptions('-vf', 'fps=16,scale=360:-1')
-                .format('gif')
-                .saveToFile(outputFile)
-                .on('end', () => {
-                    console.log('file has been converted successfully')
-                    resolve()
-                })
-                .on('error', (error: Error) => {
-                    console.error('an error happened: ' + error.message)
-                    reject(error)
-                })
+            const convert = (resolution: number, fps: number) => {
+                ffmpeg()
+                    .input(inputFile)
+                    .outputOptions('-vf', `fps=${fps},scale=${resolution}:-1`)
+                    .format('gif')
+                    .save(outputFile)
+                    .on('end', () => {
+                        console.log('file has been converted successfully')
+                        const stats = fs.statSync(outputFile)
+                        const fileSizeInBytes = stats.size
+
+                        if (fileSizeInBytes > targetFileSize) {
+                            console.log(`文件大小 ${fileSizeInBytes} 超过 1MB，重新调整参数`)
+                            if (resolution > 100 && fps > 1) {
+                                // 递归调用，降低分辨率和帧率
+                                convert(resolution - 50, fps - 1)
+                            } else {
+                                reject(new Error('无法将文件压缩到 1MB 以下'))
+                            }
+                        } else {
+                            console.log(`文件大小 ${fileSizeInBytes} 满足要求`)
+                            resolve()
+                        }
+                    })
+                    .on('error', (error: Error) => {
+                        console.error('an error happened: ' + error.message)
+                        reject(error)
+                    })
+            }
+
+            // 初始参数
+            const initialResolution = 360
+            const initialFps = 16
+
+            // 开始转换
+            convert(initialResolution, initialFps)
         })
     }
 
