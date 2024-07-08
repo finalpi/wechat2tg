7e6997d4fff6b242e5c57aca5086694f697049a0
finalpi <46010917+finalpi@users.noreply.github.com>
2024-07-08 16:54:26 +0800

    Fix: 群聊邀请消息无法正常显示

--- src/i18n/locales/en.ts
+++ src/i18n/locales/en.ts
@@ -125,7 +125,7 @@ const en = {
     },
     wechat: {
         requestAddFriend: 'Request to add you as a friend:',
-        unknownUser: 'Unknown user',
+        unknownUser: 'Unknown user:\n',
         plzViewOnPhone: 'Please view on phone',
         get: 'Received',
         getOne: 'Received one',
