cde8fce7c4545210b547ea8c330ed0bafd4fe85f
finalpi <46010917+finalpi@users.noreply.github.com>
2024-07-08 13:54:22 +0800

    Fix: message格式不生效

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -88,15 +88,15 @@ export class SimpleMessageSender implements MessageSender {
     static transformTitleStr(inputString: string, alias: string, name: string, topic: string): string {
         const alias_first = alias || name
         inputString = inputString.replace(this.NAME_REGEXP, (match, p1) => {
-            if (p1.includes('alias')){
+            if (p1.includes('alias_first')){
+                return alias_first ? p1.replaceAll('alias_first',alias_first) : ''
+            } else if (p1.includes('alias')){
                 return alias ? p1.replaceAll('alias',alias) : ''
             } else if (p1.includes('name')){
                 return name ? p1.replaceAll('name',name) : ''
             } else if (p1.includes('topic')){
                 return topic ? p1.replaceAll('topic',topic) : ''
-            } else if (p1.includes('alias_first')){
-                return alias_first ? p1.replaceAll('alias_first',alias_first) : ''
-            } else {
+            }else {
                 return match
             }
         })
