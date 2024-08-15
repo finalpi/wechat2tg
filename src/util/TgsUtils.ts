f648b7b70885e8462fae7e55406e1928e6f3d1bd
finalpi <46010917+finalpi@users.noreply.github.com>
2024-08-15 15:35:24 +0800

    WIP: build

--- src/util/TgsUtils.ts
+++ src/util/TgsUtils.ts
@@ -19,7 +19,7 @@ export default class TgsUtils {
             }
             args.push(inputFile)
             console.log('tgsToGif args: ' + args.join(' '))
-            spawn('lottie_to_gif.sh', args, {
+            spawn('./lottie_to_gif.sh', args, {
                 shell: true
             }).on('exit', code => {
                 console.log('退出代码:', code)
@@ -31,7 +31,7 @@ export default class TgsUtils {
                     args.push('--fps', '24')
                     const zoom = statSync.size / 1024 / 1024
                     console.log('tgsToGif 第二次转换 args: ' + args.join(' '))
-                    spawn('lottie_to_gif.sh', args, {
+                    spawn('./lottie_to_gif.sh', args, {
                         shell: true
                     }).on('exit', code => {
                         console.log('退出代码:', code)
