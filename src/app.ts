4379093ae41c5dcfb80e170073ded5413745fd99
xleat <xleat@outlook.com>
2024-09-22 19:09:29 +0800

    Modify: app 启动方法改名为 start

--- src/app.ts
+++ src/app.ts
@@ -5,5 +5,4 @@ process.on('uncaughtException', (err) => {
     LogUtils.config().getLogger('error').error('wechat2Tg uncaughtException', err)
 })
 
-const bot = TelegramBotClient.getInstance()
-bot.start()
+TelegramBotClient.getInstance().start()
