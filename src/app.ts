3cb1177af8a69e9ed249b190d21b19308e871c8b
finalpi <46010917+finalpi@users.noreply.github.com>
2025-01-20 11:48:40 +0800

    WIP: 修复初始化报错

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import 'reflect-metadata'
 import {AppDataSource} from './data-sourse'
 import {ClientFactory} from './client/factory/ClientFactory'
+import {ConfigurationService} from './service/ConfigurationService'
 
 AppDataSource.initialize()
     .then(async () => {
-        new ClientFactory().create('botClient').login()
+        ConfigurationService.getInstance().getConfig().then(()=>{
+            new ClientFactory().create('botClient').login()
+        })
     })
     .catch((error) => console.log(error))
