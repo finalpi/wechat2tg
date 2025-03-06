8c6374675915c2f4f188f029d7a5c4133cca411b
finalpi <46010917+finalpi@users.noreply.github.com>
2025-03-06 21:44:10 +0800

    修复bug
    修复无法登陆 tg bot 的问题

--- src/client/TelegramBotClient.ts
+++ src/client/TelegramBotClient.ts
@@ -646,17 +646,18 @@ export class TelegramBotClient extends AbstractClient {
             const messageId = ctx.message.message_id
             const chatId = ctx.chat.id
             const exist = await this.bindGroupService.getByChatId(chatId)
-            if (!exist) {
+ 
+            // 处理等待用户输入的指令
+            if (await this.dealWithCommand(ctx, text)) {
+                return
+            }
+           if (!exist) {
                 // 未绑定消息直接返回
                 return
             }
             const replyMessageId = ctx.update.message['reply_to_message']?.message_id
             // 其他 bot 的命令会进来，不处理
             if (text.startsWith('/')) {
-                return
-            }
-            // 处理等待用户输入的指令
-            if (await this.dealWithCommand(ctx, text)) {
                 return
             }
             const message: BaseMessage = {
