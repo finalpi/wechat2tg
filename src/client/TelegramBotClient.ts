80c6ccf8ff78ff4434672283c9e9b05eb4f5d2e0
hououinkami <hououinkami@gmail.com>
2025-04-28 15:51:44 +0800

    FIX: 微信链接卡片不能正常发送的问题

--- src/client/TelegramBotClient.ts
+++ src/client/TelegramBotClient.ts
@@ -648,28 +648,38 @@ export class TelegramBotClient extends AbstractClient {
             let text;
             let linkTitle;
             let linkUrl;
+            let linkDesc;
             text = ctx.message.text;
             if (ctx.message && 'entities' in ctx.message) {
                 const msgEntities = ctx.message.entities as any[];
                 if (msgEntities && msgEntities.length > 0) {
-                    const entity = msgEntities[0];
+                    let entity = msgEntities[0];
+                    for (const item of msgEntities) {
+                        // 只处理第一个链接
+                        if (item.type === 'text_link' || item.type === 'url') {
+                            entity = item;
+                            break;
+                        }
+                    }
                     
                     if (entity.type === 'text_link' && entity.url) {
                         linkTitle = ctx.message.text;
                         linkUrl = entity.url;
+                        linkDesc = "";
                     } else if (entity.type === 'url') {
-                        linkTitle = "链接";
+                        linkTitle = "非公众号链接";
                         linkUrl = ctx.message.text.substring(
                             entity.offset, 
                             entity.offset + entity.length
                         );
+                        linkDesc = linkUrl;
                     }
                     
                     if (linkTitle && linkUrl) {
                         text = new UrlLink({
                             title: linkTitle,
-                            desc: "",
-                            thumbUrl: `https://weixin.qq.com`,
+                            desc: linkDesc,
+                            thumbUrl: `https://raw.githubusercontent.com/hououinkami/docker/refs/heads/main/wx2tg/wechat.png`,
                             linkUrl: linkUrl,
                         });
                     }
