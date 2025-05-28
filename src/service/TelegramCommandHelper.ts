887f54d07adf4897d32c1ff13702f172dc1f794d
finalpi <46010917+finalpi@users.noreply.github.com>
2025-05-28 12:15:02 +0800

    feat: 好友添加功能

--- src/service/TelegramCommandHelper.ts
+++ src/service/TelegramCommandHelper.ts
@@ -12,14 +12,14 @@ export default class TgCommandHelper {
             {command: 'login', description: '登录'},
             // {command: 'flogin', description: '登录文件传输助手接收文件消息'},
             {command: 'update', description: '更新群组头像和名称'},
-            // {command: 'add', description: '根据手机号添加好友，在后面加上你需要添加用户的手机号'},
+            {command: 'add', description: '根据手机号添加好友，在后面加上你需要添加用户的手机号'},
             {command: 'message', description: '是否接收该群组消息'},
             {command: 'forward', description: '是否转发群组内其他人的消息'},
             {command: 'revoke', description: '撤回消息'},
             {command: 'settings', description: '程序设置'},
             {command: 'user', description: '查看联系人'},
             {command: 'room', description: '查看微信群'},
-            // {command: 'getqr', description: '获取我的二维码名片'},
+            {command: 'getqr', description: '获取我的二维码名片'},
             {command: 'unbind', description: '解绑群组'},
         ]
 
