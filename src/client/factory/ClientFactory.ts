10bf2aa9b32180ae546d2a95c89797b59499795f
finalpi <46010917+finalpi@users.noreply.github.com>
2025-05-30 10:38:53 +0800

    refactor: 去掉文件传输助手依赖，减少包体积

--- src/client/factory/ClientFactory.ts
+++ src/client/factory/ClientFactory.ts
@@ -4,7 +4,6 @@ import {TelegramBotClient} from '../TelegramBotClient'
 import {WeChatClient} from '../WechatClient'
 import {UserMTProtoClient} from '../UserMTProtoClient'
 import {BotMTProtoClient} from '../BotMTProtoClient'
-import {FileHelperClient} from '../FileHelperClient'
 
 export class ClientFactory implements BaseFactory {
     create(type: botType): IClient {
@@ -17,8 +16,6 @@ export class ClientFactory implements BaseFactory {
                 return UserMTProtoClient.getInstance()
             case 'botMTPClient':
                 return BotMTProtoClient.getInstance()
-            case 'fhClient':
-                return FileHelperClient.getInstance()
         }
     }
 }
