36b4fdeeaf2f8f1059ebcaf342ab4f746fad83c8
finalpi <46010917+finalpi@users.noreply.github.com>
2026-01-13 16:10:14 +0800

    chore: 调试日志

--- src/client/WechatClient.ts
+++ src/client/WechatClient.ts
@@ -347,6 +347,9 @@ export class WeChatClient extends AbstractClient {
     }
 
     async onMessage(msg: WxMessage) {
+        // 打印收到的微信消息（debug 级别）
+        this.logger.debug(`收到微信消息: type=${msg.type()}, msgId=${msg.newMsgId}, toId=${msg.toId}, text=${msg.text()?.substring(0, 100)}, xml=${msg._xml}`)
+        
         // TODO: 只处理新消息，丢弃历史消息（未来可以增加选项更好的保存聊天记录）
         if (msg.date() < this.startTime) {
             return
@@ -553,7 +556,6 @@ export class WeChatClient extends AbstractClient {
                 } else {
                     // 找不到上下文
                     msgJson = WxMessage.getXmlToJson(msg._xml)
-                    this.logger.info(`引用消息: ${JSON.stringify(msgJson)}`)
                     if (msgJson.msg.appmsg.refermsg) {
                         const referType = Number(msgJson.msg.appmsg.refermsg.type)
                         let quoteContent = ''
